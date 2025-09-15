c998f9d0830d41a8ab437bbaf40872440128a2ec
Trần Hồ Phương Nguyên <phng972005@gmail.com>
2025-09-15 09:44:13 +0700

    Initial commit

---
+++ src/main/java/vn/iotstar/entity/Category.java
@@ -0,0 +1,34 @@
+package vn.iotstar.entity;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "categories")
+public class Category implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) 
+    private int id; 
+
+    @Column(name = "name", columnDefinition = "NVARCHAR(255)") 
+    private String name;
+
+    @Column(columnDefinition = "NVARCHAR(500)")
+    private String images;
+
+    
+}
